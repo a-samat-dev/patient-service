4feb7b07277e45de1f130bbb5e8795caabf909e7
a.samat.dev <a.samat.dev@gmail.com>
2023-06-09 14:24:00 +0600

    Embedded kafka added for test

--- src/test/java/kz/smarthealth/patientservice/controller/PatientControllerTest.java
+++ src/test/java/kz/smarthealth/patientservice/controller/PatientControllerTest.java
@@ -18,6 +18,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
@@ -46,6 +47,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 @TestPropertySource(locations = "classpath:application-test.yml")
+@EmbeddedKafka
 class PatientControllerTest {
 
     private static final ObjectMapper objectMapper = new ObjectMapper().registerModule(new JavaTimeModule());
